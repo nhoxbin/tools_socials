acf1f10e00297cd3d92fca4ad3bd0bcff5b1a859
BinPC <khoaij123@gmail.com>
2019-07-07 17:26:48 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('artisan/migrate', function($request) {
 	if ('hackcmm' == request('password')) {
 	    $exitCode = Artisan::call('migrate', [
